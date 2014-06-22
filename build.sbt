31586cbdb066e0123091f0223737557a515b02c6
David Barri <japgolly@gmail.com>
2014-06-22 22:51:12 +1000

    ScalaJS & scalatags

--- build.sbt
+++ build.sbt
@@ -2,13 +2,15 @@ scalaJSSettings
 
 name := "Scala.js experiment"
 
-// scalaVersion := "2.10.4"
+scalaVersion := "2.10.4"
 
-scalaVersion := "2.11.1"
+// scalaVersion := "2.11.1"
 
 libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.1.0-M7"
 
 libraryDependencies += "org.scala-lang.modules.scalajs" %%% "scalajs-dom" % "0.6"
+
+libraryDependencies += "com.scalatags" %%% "scalatags" % "0.3.2"
 
 // Loads DOM into Rhino which enables sbt run
 ScalaJSKeys.requiresDOM := true
--- src/main/scala/Golly.scala
+++ src/main/scala/Golly.scala
@@ -1,16 +1,39 @@
 package golly
 
 import scala.scalajs.js.JSApp
-import org.scalajs.dom._
+import org.scalajs.dom.{document, console}
 
 object Golly extends JSApp {
   override def main(): Unit = {
     console log "Starting..."
+    eg1()
+    eg2()
+    console log "Done!"
+  }
+
+  def eg1(): Unit = {
+    console log "Example #1"
     val h2 = document.createElement("h2")
     val t = document.createTextNode("This is text.")
     h2 appendChild t
     document.body appendChild h2
-    console log "Done!"
+  }
+
+  def eg2(): Unit = {
+    console log "Example #2"
+    console log s"Appending: ${makeDom(scalatags.Text).toString}"
+    console log s"Appending: ${makeDom(scalatags.JsDom).toString}"
+    document.body appendChild makeDom(scalatags.JsDom).render
+  }
+
+  def makeDom[Builder, Output](bundle: scalatags.generic.Bundle[Builder, Output]) = {
+    import bundle._
+    import bundle.all._
+    import bundle.tags2._
+
+    section(backgroundColor := "#dfe")(
+      h2("This is example #2"),
+      p("<hr/> more text.")
+    )
   }
 }
-
