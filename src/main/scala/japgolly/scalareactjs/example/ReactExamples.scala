a3235669fa7aa5dc38a801601feda668e4b2ebf7
David Barri <japgolly@gmail.com>
2014-07-01 00:46:31 +1000

    Replicated examples on reactJS site

--- src/main/scala/japgolly/scalareactjs/example/ReactExamples.scala
+++ src/main/scala/japgolly/scalareactjs/example/ReactExamples.scala
@@ -1,8 +1,7 @@
 package japgolly.scalareactjs.example
 
-import scala.collection.immutable.SortedSet
 import scala.scalajs.js
-import org.scalajs.dom.{HTMLInputElement, console, document, window}
+import org.scalajs.dom.{HTMLInputElement, console, document, window, Node}
 
 import japgolly.scalareactjs._
 import vdom.ReactVDom._
@@ -11,175 +10,82 @@ import all._
 object ReactExamples extends js.JSApp {
 
   override def main(): Unit = {
-    Sample4()
+    example1(document getElementById "eg1")
+    example2(document getElementById "eg2")
+    example3(document getElementById "eg3")
   }
 
-  object Sample1 {
-
-    case class HelloProps(name: String, age: Int)
-
-    val component = ReactComponentB[HelloProps]("sample1")
-      .render(P =>
-        div(backgroundColor := "#fdd", color := "#c00")(
-          h1("THIS IS COOL."),
-          p(textDecoration := "underline")("Hello there, ", "Hello, ", P.name, " of age ", P.age)
-        ).render
-      ).build
-
-    def apply(): Unit = {
-      React.renderComponent(component.create(HelloProps("Johnhy", 100)), document getElementById "target")
-    }
-  }
-  
   // ===================================================================================================================
 
-  object Sample2 {
+  def example1(mountNode: Node) = {
 
-    case class MyProps(title: String, startTime: Long)
+    val HelloMessage = ReactComponentB[String]("HelloMessage")
+      .render(name => div("Hello ", name))
+      .create
 
-    case class MyState(secondsElapsed: Long) {
-      def inc = MyState(secondsElapsed + 1)
-    }
+    React.renderComponent(HelloMessage("John"), mountNode)
+  }
 
-    class MyBackend {
+  // ===================================================================================================================
+
+  def example2(mountNode: Node) = {
+
+    case class State(secondsElapsed: Long)
+
+    class Backend {
       var interval: js.UndefOr[Int] = js.undefined
-      def start(tick: js.Function): Unit = interval = window.setInterval(tick, 1000)
-      def stop(): Unit = interval foreach window.clearInterval
+      def tick(scope: ComponentScopeM[_, State, _]): js.Function =
+        () => scope.modState(s => State(s.secondsElapsed + 1))
     }
 
-    val component = ReactComponentB[MyProps]("sample2")
-      .getInitialState(p => MyState(p.startTime))
-      .backend(_ => new MyBackend)
-      .render((P,S,_) =>
-        div(backgroundColor := "#fdd", color := "#c00")(
-          h1("THIS IS AWESOME (", P.title, ")"),
-          p(textDecoration := "underline")("Seconds elapsed: ", S.secondsElapsed)
-        ).render
-      )
-      .componentDidMount(ctx => {
-        val tick: js.Function = (_: js.Any) => ctx.modState(_.inc)
-        console log "Installing timer..."
-        ctx.backend.start(tick)
-      })
-      .componentWillUnmount(_.backend.stop)
-      .build
+    val Timer = ReactComponentB[Unit]("Timer")
+      .initialState(State(0))
+      .backend(_ => new Backend)
+      .render((_,S,_) => div("Seconds elapsed: ", S.secondsElapsed))
+      .componentDidMount(scope =>
+        scope.backend.interval = window.setInterval(scope.backend.tick(scope), 1000))
+      .componentWillUnmount(_.backend.interval foreach window.clearInterval)
+      .create
 
-    def apply(): Unit = {
-      React.renderComponent(component.create(MyProps("Great", 0)), document getElementById "target")
-      React.renderComponent(component.create(MyProps("Again", 1000)), document getElementById "target2")
-    }
+    React.renderComponent(Timer(()), mountNode)
   }
 
   // ===================================================================================================================
 
-  object Sample3 {
+  def example3(mountNode: Node) = {
+
+    val TodoList = ReactComponentB[List[String]]("TodoList")
+      .render(P => {
+        def createItem(itemText: String) = li(itemText)
+        ul(P map createItem)
+      })
+      .create
 
     case class State(items: List[String], text: String)
 
-    val inputRef = Ref[HTMLInputElement]("i")
-
-    val TodoList = ReactComponentB[List[String]]("TodoList")
-      .render(P =>
-        ul(P.map(itemText => li(itemText))).render
-      ).build
+    class Backend(t: ComponentScopeB[Unit, State]) {
+      def onChange(e: SyntheticEvent[HTMLInputElement]) =
+        t.modState(_.copy(text = e.target.value))
+      def handleSubmit(e: SyntheticEvent[HTMLInputElement]) = {
+        e.preventDefault()
+        t.modState(s => State(s.items :+ s.text, ""))
+      }
+    }
 
     val TodoApp = ReactComponentB[Unit]("TodoApp")
-      .initialState(State(List("Sample todo #1", "Sample todo #2"), "Sample todo #3"))
+      .initialState(State(Nil, ""))
       .backend(new Backend(_))
       .render((_,S,B) =>
         div(
           h3("TODO"),
-          TodoList.create(S.items),
+          TodoList(S.items),
           form(onsubmit ==> B.handleSubmit)(
-            input(onchange ==> B.onChange, value := S.text, ref := inputRef)(),
+            input(onchange ==> B.onChange, value := S.text)(),
             button("Add #", S.items.length + 1)
           )
-        ).render
-      )
-      .build
+        )
+      ).create
 
-    class Backend(t: ComponentScopeB[Unit, State]) {
-      val handleSubmit: SyntheticEvent[HTMLInputElement] => Unit = e => {
-        e.preventDefault()
-        val nextItems = t.state.items :+ t.state.text
-        t.setState(State(nextItems, ""))
-        inputRef(t).tryFocus()
-      }
-
-      val onChange: SyntheticEvent[HTMLInputElement] => Unit = e =>
-        t.modState(_.copy(text = e.target.value))
-    }
-
-    def apply(): Unit = {
-      React.renderComponent(TodoApp.create(()), document getElementById "target")
-    }
-
-  }
-
-  // ===================================================================================================================
-
-  def textChangeRecv(f: String => Unit): SyntheticEvent[HTMLInputElement] => Unit = e => f(e.target.value)
-
-  object Sample4 {
-
-    case class State(people: SortedSet[String], text: String, focusPerson: Option[String]) {
-      def updateText(t: String) = copy(text = t, focusPerson = None)
-    }
-
-    class PeopleListBackend(t: ComponentScopeB[Unit, State]) {
-      def delete(name: String): Unit = {
-        val p = t.state.people
-        if (p.contains(name))
-          t.setState(State(p - name, name, None))
-      }
-
-      val onChange = textChangeRecv(s => t.modState(_ updateText s))
-
-      val onKP: SyntheticEvent[HTMLInputElement] => Unit =
-        e => if (e.keyboardEvent.keyCode == 13) {
-            e.preventDefault()
-            add()
-          }
-
-      def add(): Unit = t.setState(State(t.state.people + t.state.text, "", Some(t.state.text)))
-    }
-
-    case class PeopleListProps(people: SortedSet[String], latest: Option[String], deleteFn: String => Unit)
-
-    val PeopleList = {
-      val focusNext = Ref[HTMLInputElement]("latest")
-
-      ReactComponentB[PeopleListProps]("PeopleList")
-        .render(P =>
-          if (P.people.isEmpty)
-            div(color := "#800")("No people in your list!!").render
-          else
-            ol(P.people.toList.map(p =>
-              li(
-                input(value := p, (P.latest contains p) && (ref := focusNext))(),
-                button(marginLeft := 1.em, onclick runs P.deleteFn(p))("Delete"))
-            )).render
-          )
-          .componentDidUpdate((t,_,_) => focusNext(t).tryFocus())
-          .componentDidMount(t => focusNext(t).tryFocus())
-          .build
-    }
-
-    val PeopleEditor = ReactComponentB[Unit]("PeopleEditor")
-      .getInitialState(_ => State(SortedSet("First","Second", "x"), "Middle", Some("Second")))
-      .backend(new PeopleListBackend(_))
-      .render((_,S,B) =>
-          div(
-            h3("People List")
-            ,div(PeopleList.create(PeopleListProps(S.people, S.focusPerson, B.delete)))
-            ,h3("Add")
-            ,input(onchange ==> B.onChange, onkeypress ==> B.onKP, value := S.text)()
-            ,button(onclick runs B.add())("+")
-          ).render
-      )
-      .build
-
-    def apply(): Unit =
-      React.renderComponent(PeopleEditor.create(()), document getElementById "target2")
+    React.renderComponent(TodoApp(()), mountNode)
   }
 }
