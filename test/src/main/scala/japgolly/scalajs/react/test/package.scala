dda74a146619a5c14c1a48fbcc51638a0ad546ba
David Barri <japgolly@gmail.com>
2014-10-16 19:06:58 +1100

    auto unbox refs in tests

--- test/src/main/scala/japgolly/scalajs/react/test/package.scala
+++ test/src/main/scala/japgolly/scalajs/react/test/package.scala
@@ -1,6 +1,6 @@
 package japgolly.scalajs.react
 
-import scala.scalajs.js.Object
+import scala.scalajs.js.{Object, UndefOr}
 
 package object test {
 
@@ -10,6 +10,9 @@ package object test {
   @inline final implicit def RTUSChangeEventData  (d: ChangeEventData  ): Object = d.toJs
   @inline final implicit def RTUSKeyboardEventData(d: KeyboardEventData): Object = d.toJs
   @inline final implicit def RTUSMouseEventData   (d: MouseEventData   ): Object = d.toJs
+
+  @inline final implicit def autoUnboxRefsInTests[T <: TopNode](r: UndefOr[ReactComponentM_[T]]) = r.get
+  @inline final implicit def autoUnboxRefsInTestsC[T <: TopNode](r: UndefOr[ReactComponentM_[T]]): ComponentOrNode = r.get
 
 //  implicit final class RTUSimulateExt(val u: Simulate) extends AnyVal {
 //    def change(t: ComponentOrNode, newValue: String) = u.change(t, ChangeEventData(value = newValue))
