1a91e3f2b445838665bc26f9ea76411efcdf2dd4
David Barri <japgolly@gmail.com>
2014-12-05 18:30:17 +1100

    Refresh gh-pages

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -3,12 +3,16 @@ cd "$(dirname "$0")" || exit 1
 
 ii=gh-pages/index.html
 io=index.html
+js=gh-pages/res/ghpages.js
 
-rm -f $io
+rm -f $io $js
 
 cat $ii | perl -pe '
     s!"res/!"gh-pages/res/!g;
-    s!"target/.+?\.js"!"gh-pages/res/ghpages.js"!g;
+    s!"target/.+?\.js"!"'"$js"'"!g;
     ' > $io \
-  && sbt gh-pages/fullOptJS
+  && sbt gh-pages/fullOptJS \
+  && git add $js $io \
+  && git st \
+  && echo "git commit -m 'Refresh gh-pages'" && echo
 
