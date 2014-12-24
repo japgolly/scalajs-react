1dce6e8500b69e49b608b2886432d977b14d76f0
David Barri <japgolly@gmail.com>
2014-12-24 14:30:09 +1100

    Tweaked LogLifecycle format

--- extras/src/main/scala/japgolly/scalajs/react/extras/LogLifecycle.scala
+++ extras/src/main/scala/japgolly/scalajs/react/extras/LogLifecycle.scala
@@ -10,41 +10,44 @@ import scala.scalajs.js
  * Install in `ReactComponentB` via `.configure(LogLifecycle.short)` or `.configure(LogLifecycle.verbose)`.
  */
 object LogLifecycle {
-  @inline private[this] def fmt(m: String, a: Any) =
+  private[this] def header(name: String): String => String =
+    h => s"[$name] $h"
+
+  private[this] def fmt(m: String, a: Any) =
     Seq[js.Any](s"\n  $m: $a")
 
-  @inline private[this] def log(m: js.Any, ps: js.Any*) =
+  private[this] def log(m: js.Any, ps: js.Any*) =
     console.log(m, ps: _*)
 
-  @inline private[this] def logc(m: js.Any, c: js.Any, ps: js.Any*) =
+  private[this] def logc(m: js.Any, c: js.Any, ps: js.Any*) =
     log(m + "\n ", c +: ps: _*)
 
-  @inline private[this] def log1(m: String) = (c: js.Any) =>
+  private[this] def log1(m: String) = (c: js.Any) =>
     logc(m, c)
 
-  @inline private[this] def logp(m: String) = (c: js.Any, p: Any) =>
+  private[this] def logp(m: String) = (c: js.Any, p: Any) =>
     logc(m, c, fmt("Props", p): _*)
 
-  @inline private[this] def logps(m: String) = (c: js.Any, p: Any, s: Any) =>
+  private[this] def logps(m: String) = (c: js.Any, p: Any, s: Any) =>
     logc(m, c, fmt("Props", p) ++ fmt("State", s): _*)
 
   def short[P, S, B] = (rc: ReactComponentB[P, S, B]) => {
-    val name = rc.name
-    rc.componentWillMount       (_       => log(s"$name.componentWillMount"))
-      .componentDidMount        (_       => log(s"$name.componentDidMount"))
-      .componentWillUnmount     (_       => log(s"$name.componentWillUnmount"))
-      .componentWillUpdate      ((_,_,_) => log(s"$name.componentWillUpdate"))
-      .componentDidUpdate       ((_,_,_) => log(s"$name.componentDidUpdate"))
-      .componentWillReceiveProps((_,_)   => log(s"$name.componentWillReceiveProps"))
+    val h = header(rc.name)
+    rc.componentWillMount       (_       => log(h("componentWillMount")))
+      .componentDidMount        (_       => log(h("componentDidMount")))
+      .componentWillUnmount     (_       => log(h("componentWillUnmount")))
+      .componentWillUpdate      ((_,_,_) => log(h("componentWillUpdate")))
+      .componentDidUpdate       ((_,_,_) => log(h("componentDidUpdate")))
+      .componentWillReceiveProps((_,_)   => log(h("componentWillReceiveProps")))
   }
 
   def verbose[P, S, B] = (rc: ReactComponentB[P, S, B]) => {
-    val name = rc.name
-    rc.componentWillMount       (log1 (s"$name.componentWillMount"))
-      .componentDidMount        (log1 (s"$name.componentDidMount"))
-      .componentWillUnmount     (log1 (s"$name.componentWillUnmount"))
-      .componentWillUpdate      (logps(s"$name.componentWillUpdate"))
-      .componentDidUpdate       (logps(s"$name.componentDidUpdate"))
-      .componentWillReceiveProps(logp (s"$name.componentWillReceiveProps"))
+    val h = header(rc.name)
+    rc.componentWillMount       (log1 (h("componentWillMount")))
+      .componentDidMount        (log1 (h("componentDidMount")))
+      .componentWillUnmount     (log1 (h("componentWillUnmount")))
+      .componentWillUpdate      (logps(h("componentWillUpdate")))
+      .componentDidUpdate       (logps(h("componentDidUpdate")))
+      .componentWillReceiveProps(logp (h("componentWillReceiveProps")))
   }
 }
