12710072d6e8c6917459e0a36906ca746af2995c
David Barri <japgolly@gmail.com>
2015-05-25 11:38:28 +1000

    Refactor: RouterCtl gets own file

--- extra/src/main/scala/japgolly/scalajs/react/extra/router2/Router.scala
+++ extra/src/main/scala/japgolly/scalajs/react/extra/router2/Router.scala
@@ -4,8 +4,7 @@ import org.scalajs.dom
 import scala.scalajs.js
 import scalaz.{\/-, -\/, \/, ~>, Free}
 import scalaz.effect.IO
-import scalaz.syntax.bind.ToBindOps
-import japgolly.scalajs.react._, vdom.prefix_<^._, ScalazReact._
+import japgolly.scalajs.react._, ScalazReact._
 import japgolly.scalajs.react.extra._
 
 object Router {
@@ -164,48 +163,4 @@ final class RouterLogic[Page](val baseUrl: BaseUrl, cfg: RouterConfig[Page]) ext
 
   val ctl: RouterCtl[Page] =
     ctlByPath contramap cfg.path
-}
-
-/**
- * Router controller. A client API to the router.
- *
- * @tparam A A data type that indicates a route that can be navigated to.
- */
-abstract class RouterCtl[A] {
-  def baseUrl: BaseUrl
-  def byPath: RouterCtl[Path]
-  def refresh: IO[Unit]
-  def pathFor(target: A): Path
-  def set(target: A): IO[Unit]
-
-  final def urlFor(target: A): AbsUrl =
-    pathFor(target).abs(baseUrl)
-
-  final def setEH(target: A): ReactEvent => IO[Unit] =
-    e => preventDefaultIO(e) >> stopPropagationIO(e) >> set(target)
-
-  final def setOnClick(target: A): TagMod =
-    ^.onClick ~~> setEH(target)
-
-  final def link(target: A): ReactTag =
-    <.a(^.href := urlFor(target).value, setOnClick(target))
-
-  final def contramap[B](f: B => A): RouterCtl[B] =
-    new RouterCtl.Contramap(this, f)
-
-  final def narrow[B <: A]: RouterCtl[B] =
-    contramap(b => b)
-}
-
-object RouterCtl {
-  implicit def reusability[A]: Reusability[RouterCtl[A]] =
-    Reusability.byRef
-
-  class Contramap[A, B](u: RouterCtl[A], f: B => A) extends RouterCtl[B] {
-    override def baseUrl       = u.baseUrl
-    override def byPath        = u.byPath
-    override def refresh       = u.refresh
-    override def pathFor(b: B) = u pathFor f(b)
-    override def set(b: B)     = u set f(b)
-  }
 }
---
+++ extra/src/main/scala/japgolly/scalajs/react/extra/router2/RouterCtl.scala
@@ -0,0 +1,52 @@
+package japgolly.scalajs.react.extra.router2
+
+import scalaz.effect.IO
+import scalaz.syntax.bind.ToBindOps
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.ScalazReact._
+import japgolly.scalajs.react.extra.Reusability
+import japgolly.scalajs.react.vdom.prefix_<^._
+
+/**
+ * Router controller. A client API to the router.
+ *
+ * @tparam A A data type that indicates a route that can be navigated to.
+ */
+abstract class RouterCtl[A] {
+  def baseUrl: BaseUrl
+  def byPath: RouterCtl[Path]
+  def refresh: IO[Unit]
+  def pathFor(target: A): Path
+  def set(target: A): IO[Unit]
+
+  final def urlFor(target: A): AbsUrl =
+    pathFor(target).abs(baseUrl)
+
+  final def setEH(target: A): ReactEvent => IO[Unit] =
+    e => preventDefaultIO(e) >> stopPropagationIO(e) >> set(target)
+
+  final def setOnClick(target: A): TagMod =
+    ^.onClick ~~> setEH(target)
+
+  final def link(target: A): ReactTag =
+    <.a(^.href := urlFor(target).value, setOnClick(target))
+
+  final def contramap[B](f: B => A): RouterCtl[B] =
+    new RouterCtl.Contramap(this, f)
+
+  final def narrow[B <: A]: RouterCtl[B] =
+    contramap(b => b)
+}
+
+object RouterCtl {
+  implicit def reusability[A]: Reusability[RouterCtl[A]] =
+    Reusability.byRef
+
+  class Contramap[A, B](u: RouterCtl[A], f: B => A) extends RouterCtl[B] {
+    override def baseUrl       = u.baseUrl
+    override def byPath        = u.byPath
+    override def refresh       = u.refresh
+    override def pathFor(b: B) = u pathFor f(b)
+    override def set(b: B)     = u set f(b)
+  }
+}
