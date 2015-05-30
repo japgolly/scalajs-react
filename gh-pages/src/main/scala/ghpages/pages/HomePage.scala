600829784108649c00790c69aebb319db6c824df
David Barri <japgolly@gmail.com>
2015-05-30 14:10:52 +1000

    gh-pages: Update copy

--- gh-pages/src/main/scala/ghpages/pages/HomePage.scala
+++ gh-pages/src/main/scala/ghpages/pages/HomePage.scala
@@ -7,6 +7,9 @@ import japgolly.scalajs.react._, vdom.prefix_<^._
  */
 object HomePage {
 
+  private val p =
+    <.p(^.margin := "1.3em 0")
+
   val component = ReactComponentB.static("Home",
     <.div(
       <.h1(
@@ -14,22 +17,37 @@ object HomePage {
           ^.color := "#000",
           ^.href  := "https://github.com/japgolly/scalajs-react",
           "scalajs-react")),
+
       <.section(
-        ^.marginTop := "1.6em",
+        ^.marginTop := "2.2em",
         ^.fontSize  := "115%",
-        ^.color     := "#444",
-        <.p("Lifts Facebook's ",
+        ^.color     := "#333",
+
+        p(
+          "Lifts Facebook's ",
           <.a(^.href := "https://facebook.github.io/react", "React"),
           " library into ",
           <.a(^.href := "http://www.scala-js.org", "Scala.js"),
           " and endeavours to make it as type-safe and Scala-friendly as possible."),
-        <.p(
-          "In addition to wrapping React, this provides extra opt-in functionality to support (separately) easier testing, and pure FP.")),
+
+        p(
+          "Provides (opt-in) support for pure functional programming, using ",
+          <.a(^.href := "https://github.com/scalaz/scalaz", "Scalaz"),
+          " and ",
+          <.a(^.href := "https://github.com/julien-truffaut/Monocle", "Monocle"),
+          "."),
+
+        p(
+          "Comes utility modules helpful for React in Scala(.js), rather than React in JS.",
+          "Includes a router, testing utils, performance utils, more."),
+
       <.p(
-        ^.marginTop := "3em",
+        ^.fontSize  := "85%",
+        ^.marginTop := "3.3em",
         ^.fontStyle := "italic",
+        ^.color     := "#444",
         "Big thanks to ",
         <.a(^.href := "https://twitter.com/chandu0101", "@chandu0101"),
-        " for creating these pages."))
+        " for creating these pages.")))
   ).buildU
 }
