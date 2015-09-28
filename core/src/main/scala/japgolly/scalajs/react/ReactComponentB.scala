e0692750941aa9b625d00e490947edb946c4dde3
David Barri <japgolly@gmail.com>
2015-09-28 10:25:27 +1000

    Add ScalaDoc to lifecyle methods

--- core/src/main/scala/japgolly/scalajs/react/ReactComponentB.scala
+++ core/src/main/scala/japgolly/scalajs/react/ReactComponentB.scala
@@ -178,51 +178,113 @@ final class ReactComponentB[P,S,B,N <: TopNode](val name: String,
     lc.copy(getDefaultProps = p)
 
 
+  /**
+   * Invoked once, both on the client and server, immediately before the initial rendering occurs.
+   * If you call `setState` within this method, `render()` will see the updated state and will be executed only once
+   * despite the state change.
+   */
   def componentWillMount(f: DuringCallbackU[P, S, B] => Callback): ReactComponentB[P, S, B, N] =
     lc.copy(componentWillMount = fcUnit(lc.componentWillMount, f))
 
+  /**
+   * Invoked once, only on the client (not on the server), immediately after the initial rendering occurs. At this point
+   * in the lifecycle, the component has a DOM representation which you can access via `React.findDOMNode(this)`.
+   * The `componentDidMount()` method of child components is invoked before that of parent components.
+   *
+   * If you want to integrate with other JavaScript frameworks, set timers using `setTimeout` or `setInterval``, or send
+   * AJAX requests, perform those operations in this method.
+   */
   def componentDidMount(f: DuringCallbackM[P, S, B, N] => Callback): ReactComponentB[P, S, B, N] =
     lc.copy(componentDidMount = fcUnit(lc.componentDidMount, f))
 
+  /**
+   * Invoked immediately before a component is unmounted from the DOM.
+   *
+   * Perform any necessary cleanup in this method, such as invalidating timers or cleaning up any DOM elements that were
+   * created in `componentDidMount`.
+   */
   def componentWillUnmount(f: DuringCallbackM[P, S, B, N] => Callback): ReactComponentB[P, S, B, N] =
     lc.copy(componentWillUnmount = fcUnit(lc.componentWillUnmount, f))
 
+  /**
+   * Invoked immediately before rendering when new props or state are being received. This method is not called for the
+   * initial render.
+   *
+   * Use this as an opportunity to perform preparation before an update occurs.
+   *
+   * Note: You *cannot* use `this.setState()` in this method. If you need to update state in response to a prop change,
+   * use `componentWillReceiveProps` instead.
+   */
   def componentWillUpdate(f: (WillUpdate[P, S, B, N], P, S) => Callback): ReactComponentB[P, S, B, N] =
     lc.copy(componentWillUpdate = fcUnit(lc.componentWillUpdate, f))
 
+  /**
+   * Invoked immediately after the component's updates are flushed to the DOM. This method is not called for the initial
+   * render.
+   *
+   * Use this as an opportunity to operate on the DOM when the component has been updated.
+   */
   def componentDidUpdate(f: (DuringCallbackM[P, S, B, N], P, S) => Callback): ReactComponentB[P, S, B, N] =
     lc.copy(componentDidUpdate = fcUnit(lc.componentDidUpdate, f))
 
+  /**
+   * Invoked when a component is receiving new props. This method is not called for the initial render.
+   *
+   * Use this as an opportunity to react to a prop transition before `render()` is called by updating the state using
+   * `this.setState()`. The old props can be accessed via `this.props`. Calling `this.setState()` within this function
+   * will not trigger an additional render.
+   *
+   * Note: There is no analogous method `componentWillReceiveState`. An incoming prop transition may cause a state
+   * change, but the opposite is not true. If you need to perform operations in response to a state change, use
+   * `componentWillUpdate`.
+   */
   def componentWillReceiveProps(f: (DuringCallbackM[P, S, B, N], P) => Callback): ReactComponentB[P, S, B, N] =
     lc.copy(componentWillReceiveProps = fcUnit(lc.componentWillReceiveProps, f))
 
-  def componentWillMountCB(cb: Callback): ReactComponentB[P, S, B, N] =
-      componentWillMount(_ => cb)
-
-  def componentDidMountCB(cb: Callback): ReactComponentB[P, S, B, N] =
-      componentDidMount(_ => cb)
-
-  def componentWillUnmountCB(cb: Callback): ReactComponentB[P, S, B, N] =
-      componentWillUnmount(_ => cb)
-
-  def componentWillUpdateCB(cb: Callback): ReactComponentB[P, S, B, N] =
-      componentWillUpdate((_, _, _) => cb)
-
-  def componentDidUpdateCB(cb: Callback): ReactComponentB[P, S, B, N] =
-      componentDidUpdate((_, _, _) => cb)
-
-  def componentWillReceivePropsCB(cb: Callback): ReactComponentB[P, S, B, N] =
-      componentWillReceiveProps((_, _) => cb)
-
-
+  /**
+   * Invoked before rendering when new props or state are being received. This method is not called for the initial
+   * render or when `forceUpdate` is used.
+   *
+   * Use this as an opportunity to `return false` when you're certain that the transition to the new props and state
+   * will not require a component update.
+   *
+   * If `shouldComponentUpdate` returns false, then `render()` will be completely skipped until the next state change.
+   * In addition, `componentWillUpdate` and `componentDidUpdate` will not be called.
+   *
+   * By default, `shouldComponentUpdate` always returns `true` to prevent subtle bugs when `state` is mutated in place,
+   * but if you are careful to always treat `state` as immutable and to read only from `props` and `state` in `render()`
+   * then you can override `shouldComponentUpdate` with an implementation that compares the old props and state to their
+   * replacements.
+   *
+   * If performance is a bottleneck, especially with dozens or hundreds of components, use `shouldComponentUpdate` to
+   * speed up your app.
+   */
   def shouldComponentUpdate(f: (DuringCallbackM[P, S, B, N], P, S) => Boolean): ReactComponentB[P, S, B, N] =
     shouldComponentUpdateCB(($, p, s) => CallbackTo(f($, p, s)))
 
-  def shouldComponentUpdateCB(f: (DuringCallbackM[P, S, B, N], P, S) => CallbackB): ReactComponentB[P, S, B, N] =
-    lc.copy(shouldComponentUpdate = fcEither(lc.shouldComponentUpdate, f))
-
   def shouldComponentUpdateConst(f: => Boolean): ReactComponentB[P, S, B, N] =
     shouldComponentUpdateConstCB(CallbackTo(f))
+
+  def componentWillMountCB(cb: Callback): ReactComponentB[P, S, B, N] =
+    componentWillMount(_ => cb)
+
+  def componentDidMountCB(cb: Callback): ReactComponentB[P, S, B, N] =
+    componentDidMount(_ => cb)
+
+  def componentWillUnmountCB(cb: Callback): ReactComponentB[P, S, B, N] =
+    componentWillUnmount(_ => cb)
+
+  def componentWillUpdateCB(cb: Callback): ReactComponentB[P, S, B, N] =
+    componentWillUpdate((_, _, _) => cb)
+
+  def componentDidUpdateCB(cb: Callback): ReactComponentB[P, S, B, N] =
+    componentDidUpdate((_, _, _) => cb)
+
+  def componentWillReceivePropsCB(cb: Callback): ReactComponentB[P, S, B, N] =
+    componentWillReceiveProps((_, _) => cb)
+
+  def shouldComponentUpdateCB(f: (DuringCallbackM[P, S, B, N], P, S) => CallbackB): ReactComponentB[P, S, B, N] =
+    lc.copy(shouldComponentUpdate = fcEither(lc.shouldComponentUpdate, f))
 
   def shouldComponentUpdateConstCB(f: CallbackB): ReactComponentB[P, S, B, N] =
     shouldComponentUpdateCB((_, _, _) => f)
