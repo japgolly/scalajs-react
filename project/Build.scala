efe670409dde3c268ad072ea43deaf773cfecffd
David Barri <japgolly@gmail.com>
2015-10-07 23:26:43 +1100

    Release 0.10.0-M2

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,7 @@ object ScalajsReact extends Build {
     _.enablePlugins(ScalaJSPlugin)
       .settings(
         organization       := "com.github.japgolly.scalajs-react",
-        version            := "0.10.0-SNAPSHOT",
+        version            := "0.10.0-M2",
         homepage           := Some(url("https://github.com/japgolly/scalajs-react")),
         licenses           += ("Apache-2.0", url("http://opensource.org/licenses/Apache-2.0")),
         scalaVersion       := Scala211,
