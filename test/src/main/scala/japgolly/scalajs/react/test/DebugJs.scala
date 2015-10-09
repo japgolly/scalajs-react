1812697f6ca17a91cb361de39340087cceae0246
David Barri <japgolly@gmail.com>
2015-10-09 19:30:13 +1100

    DebugJs.inspectObject: Handle no props

--- test/src/main/scala/japgolly/scalajs/react/test/DebugJs.scala
+++ test/src/main/scala/japgolly/scalajs/react/test/DebugJs.scala
@@ -15,19 +15,23 @@ object DebugJs {
 
   def inspectObject(o: Object): String = {
     val s = objectPropValues(o).sortBy(_._1)
-    val ss = s.map { case (k, v) =>
-      (k, js.typeOf(v), ("" + v).split('\n')(0))
+    if (s.isEmpty)
+      "Value has no object properties: " + o
+    else {
+      val ss = s.map { case (k, v) =>
+        (k, js.typeOf(v), ("" + v).split('\n')(0))
+      }
+      val sz = s.size
+      val nlen = sz.toString.length
+      val klen = ss.map(_._1.length).max
+      val tlen = ss.map(_._2.length).max
+      val fmt = s"  [%${nlen}d/$sz] %-${klen}s : %-${tlen}s = %s"
+      var i = 0
+      ss.map { case (k, t, v) =>
+        i = i + 1
+        fmt.format(i, k, t, v)
+      }.mkString(s"$o\n", "\n", "")
     }
-    val sz = s.size
-    val nlen = sz.toString.length
-    val klen = ss.map(_._1.length).max
-    val tlen = ss.map(_._2.length).max
-    val fmt = s"  [%${nlen}d/$sz] %-${klen}s : %-${tlen}s = %s"
-    var i = 0
-    ss.map { case (k, t, v) =>
-      i = i + 1
-      fmt.format(i, k, t, v)
-    }.mkString(s"$o\n", "\n", "")
   }
 
 }
