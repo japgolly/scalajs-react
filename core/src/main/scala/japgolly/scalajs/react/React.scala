177faf87d58095b63f50178952f52d25f19491a0
David Barri <japgolly@gmail.com>
2015-10-18 16:22:30 +1100

    Add ScalaDoc back to ReactDOMServer methods

--- core/src/main/scala/japgolly/scalajs/react/React.scala
+++ core/src/main/scala/japgolly/scalajs/react/React.scala
@@ -91,8 +91,24 @@ trait React extends Object {
   @deprecated("As of React 0.14, you must use ReactDOMServer.renderToStaticMarkup instead.", "0.10.0")
   def renderToStaticMarkup(e: ReactElement): String = js.native
   */
+
+  /**
+   * Render a ReactElement to its initial HTML. This should only be used on the server. React will return an HTML
+   * string. You can use this method to generate HTML on the server and send the markup down on the initial request for
+   * faster page loads and to allow search engines to crawl your pages for SEO purposes.
+   *
+   * If you call `ReactDOM.render()` on a node that already has this server-rendered markup, React will preserve it and
+   * only attach event handlers, allowing you to have a very performant first-load experience.
+   */
   def renderToString(e: ReactElement): String = js.native
+
+  /**
+   * Similar to [[renderToString]], except this doesn't create extra DOM attributes such as `data-react-id`, that React
+   * uses internally. This is useful if you want to use React as a simple static page generator, as stripping away the
+   * extra attributes can save lots of bytes.
+   */
   def renderToStaticMarkup(e: ReactElement): String = js.native
+
   // ===================================================================================================================
 }
 
