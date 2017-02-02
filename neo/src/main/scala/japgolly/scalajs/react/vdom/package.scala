66b8b1a88658f055a20fa039902cb9697dbce260
David Barri <japgolly@gmail.com>
2017-02-02 19:49:09 +1100

    Delete neo

--- neo/src/main/scala/japgolly/scalajs/react/vdom/package.scala
+++
@@ -1,9 +0,0 @@
-package japgolly.scalajs.react
-
-import org.scalajs.dom
-
-package object vdom {
-  type TopNode     = dom.Node
-  type HtmlTopNode = dom.html.Element
-  type SvgTopNode  = dom.svg.Element
-}
