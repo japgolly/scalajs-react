30154d793d4737153913b89ed8c2a016a4342291
David Barri <japgolly@gmail.com>
2017-02-18 15:36:45 +1100

    Tweak StateAccess

--- extra/src/main/scala/japgolly/scalajs/react/extra/StateAccess.scala
+++ extra/src/main/scala/japgolly/scalajs/react/extra/StateAccess.scala
@@ -9,18 +9,25 @@ import japgolly.scalajs.react.internal.Effect
 object StateAccess extends StateAccessImplicits {
 
   class WriteCB[-I, S](
-    final val setState: I => S => Callback,
     final val setStateCB: I => (S, Callback) => Callback,
-    final val modState: I => (S => S) => Callback,
-    final val modStateCB: I => ((S => S), Callback) => Callback)
+    final val modStateCB: I => ((S => S), Callback) => Callback) {
+
+    final val setState: I => S => Callback = i => {
+      val f = setStateCB(i)
+      f(_, Callback.empty)
+    }
+
+    final val modState: I => (S => S) => Callback = i => {
+      val f = modStateCB(i)
+      f(_, Callback.empty)
+    }
+  }
 
   class ReadFWriteCB[F[_], -I, S](
     final val state: I => F[S],
-    setState: I => S => Callback,
     setStateCB: I => (S, Callback) => Callback,
-    modState: I => (S => S) => Callback,
     modStateCB: I => ((S => S), Callback) => Callback)
-    extends WriteCB[I, S](setState, setStateCB, modState, modStateCB)
+    extends WriteCB[I, S](setStateCB, modStateCB)
 
   type ReadIdWriteCB[-I, S] = ReadFWriteCB[Effect.Id, I, S]
   type ReadCBWriteCB[-I, S] = ReadFWriteCB[CallbackTo, I, S]
@@ -36,7 +43,7 @@ sealed trait StateAccessImplicits1 {
 
   // WriteCB -- ScalaComponent.Lifecycle.StateW
   private[this] val _scalaLifecycleW = new WriteCB[ScalaComponent.Lifecycle.StateW[_, X, _], X](
-    i => i.setState(_), i => i.setState(_, _), i => i.modState(_), i => i.modState(_, _))
+    i => i.setState(_, _), i => i.modState(_, _))
   implicit def scalaLifecycleW[S]: WriteCB[ScalaComponent.Lifecycle.StateW[_, S, _], S] =
     castW(_scalaLifecycleW)
 }
@@ -47,7 +54,7 @@ sealed trait StateAccessImplicits2 extends StateAccessImplicits1 {
 
   // ReadCBWriteCB -- GenericComponent.BaseMounted[CallbackTo
   private[this] val _mountedCB = new ReadFWriteCB[CallbackTo, GenericComponent.BaseMounted[CallbackTo, _, X, _, _], X](
-    _.state, i => i.setState(_), i => i.setState(_, _), i => i.modState(_), i => i.modState(_, _))
+    _.state, i => i.setState(_, _), i => i.modState(_, _))
   implicit def mountedCB[S]: ReadCBWriteCB[GenericComponent.BaseMounted[CallbackTo, _, S, _, _], S] =
     castRW(_mountedCB)
 }
@@ -57,9 +64,7 @@ sealed trait StateAccessImplicits3 extends StateAccessImplicits2 {
   // ReadCBWriteCB -- GenericComponent.BaseMounted[Id
   private[this] lazy val _mountedIdCB = new ReadFWriteCB[CallbackTo, GenericComponent.BaseMounted[Effect.Id, _, X, _, _], X](
     i => CallbackTo(i.state),
-    i => s => Callback(i.setState(s)),
     i => (s, cb) => Callback(i.setState(s, cb)),
-    i => f => Callback(i.modState(f)),
     i => (f, cb) => Callback(i.modState(f, cb)))
   implicit def mountedIdCB[S]: ReadCBWriteCB[GenericComponent.BaseMounted[Effect.Id, _, S, _, _], S] =
     castRW(_mountedIdCB)
@@ -70,16 +75,14 @@ sealed trait StateAccessImplicits extends StateAccessImplicits3 {
   // ReadIdWriteCB -- GenericComponent.BaseMounted[Id
   private[this] val _mountedId = new ReadFWriteCB[Effect.Id, GenericComponent.BaseMounted[Effect.Id, _, X, _, _], X](
     _.state,
-    i => s => Callback(i.setState(s)),
     i => (s, cb) => Callback(i.setState(s, cb)),
-    i => f => Callback(i.modState(f)),
     i => (f, cb) => Callback(i.modState(f, cb)))
   implicit def mountedId[S]: ReadIdWriteCB[GenericComponent.BaseMounted[Effect.Id, _, S, _, _], S] =
     castRW(_mountedId)
 
   // ReadIdWriteCB -- ScalaComponent.Lifecycle.StateRW
   private[this] val _scalaLifecycleRW = new ReadFWriteCB[Effect.Id, ScalaComponent.Lifecycle.StateRW[_, X, _], X](
-    _.state, i => i.setState(_), i => i.setState(_, _), i => i.modState(_), i => i.modState(_, _))
+    _.state, i => i.setState(_, _), i => i.modState(_, _))
   implicit def scalaLifecycleRW[S]: ReadIdWriteCB[ScalaComponent.Lifecycle.StateRW[_, S, _], S] =
     castRW(_scalaLifecycleRW)
 }
