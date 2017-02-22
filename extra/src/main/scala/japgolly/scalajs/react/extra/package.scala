d46fab5380385946567607b6200d42ccd3d0eb9d
David Barri <japgolly@gmail.com>
2017-02-22 12:10:03 +1100

    Move DOM extensions into core

--- extra/src/main/scala/japgolly/scalajs/react/extra/package.scala
+++ extra/src/main/scala/japgolly/scalajs/react/extra/package.scala
@@ -1,8 +1,5 @@
 package japgolly.scalajs.react
 
-import org.scalajs.dom
-import org.scalajs.dom.html
-
 package object extra {
 
   type ~=>[-A, +B] = ReusableFn[A, B]
@@ -10,22 +7,6 @@ package object extra {
   @inline implicit final class ReactExtrasExt_Any[A](private val self: A) extends AnyVal {
     @inline def ~=~(a: A)(implicit r: Reusability[A]): Boolean = r.test(self, a)
     @inline def ~/~(a: A)(implicit r: Reusability[A]): Boolean = !r.test(self, a)
-  }
-
-  /** Extensions to plain old DOM. */
-  @inline implicit final class ReactExtrasExt_DomNode(private val n: dom.raw.Node) extends AnyVal {
-
-    @inline def domCast[N <: dom.raw.Node]: N =
-      n.asInstanceOf[N]
-
-    @inline def domAsHtml: html.Element =
-      domCast
-
-    def domToHtml: Option[html.Element] =
-      n match {
-        case e: html.Element => Some(e)
-        case _ => None
-      }
   }
 
 }
