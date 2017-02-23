0e0de59a6da52f67712952a087504b29f154d5a9
David Barri <japgolly@gmail.com>
2017-02-23 19:46:46 +1100

    Restore React.addons.CSSTransitionGroup

--- core/src/main/scala/japgolly/scalajs/react/ReactAddons.scala
+++ core/src/main/scala/japgolly/scalajs/react/ReactAddons.scala
@@ -1,5 +1,18 @@
 package japgolly.scalajs.react
 
+import scala.scalajs.js
+
 object ReactAddons {
+
   def Perf = raw.Perf
+
+  lazy val CSSTransitionGroup =
+    JsComponent[raw.ReactCSSTransitionGroupProps, Children.Varargs, Null](
+      js.Dynamic.global.React.addons.CSSTransitionGroup)
+
+  @inline def CSSTransitionGroupProps(): raw.ReactCSSTransitionGroupProps =
+    (new js.Object).asInstanceOf[raw.ReactCSSTransitionGroupProps]
+
+  @inline def CSSTransitionGroupNames(): raw.ReactCSSTransitionGroupNames =
+    (new js.Object).asInstanceOf[raw.ReactCSSTransitionGroupNames]
 }
