bae150bd3049f3210499c43b18643cd166167a2f
Alexej Haak <alexej.haak@outlook.de>
2017-03-13 22:07:24 +0100

    Make Callback.when lazy

--- core/src/main/scala/japgolly/scalajs/react/Callback.scala
+++ core/src/main/scala/japgolly/scalajs/react/Callback.scala
@@ -91,7 +91,7 @@ object Callback {
    * @param cond The condition required to be `true` for the callback to execute.
    */
   def when(cond: Boolean)(c: => Callback): Callback =
-    if (cond) c else Callback.empty
+    Callback.lazily(if (cond) c else Callback.empty)
 
   /**
    * Convenience for applying a condition to a callback, and returning `Callback.empty` when the condition is already
