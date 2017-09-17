ca74856f6069f32cb422742adc717bdf67944d08
David Barri <japgolly@gmail.com>
2017-09-18 07:39:06 +1000

    Upgrade to Scala 2.12.3

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object ScalajsReact {
 
   object Ver {
     val Scala211      = "2.11.11"
-    val Scala212      = "2.12.2"
+    val Scala212      = "2.12.3"
     val ScalaJsDom    = "0.9.3"
     val ReactJs       = "15.6.1"
     val Monocle       = "1.4.0"
