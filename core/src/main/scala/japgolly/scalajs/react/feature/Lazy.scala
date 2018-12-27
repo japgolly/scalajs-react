6d1b1063510abede60bb08cd35fa06475ac3788e
David Barri <japgolly@gmail.com>
2018-12-27 20:12:21 +1100

    Integrate AsyncCallback into Lazy

--- core/src/main/scala/japgolly/scalajs/react/feature/Lazy.scala
+++ core/src/main/scala/japgolly/scalajs/react/feature/Lazy.scala
@@ -2,21 +2,18 @@ package japgolly.scalajs.react.feature
 
 import japgolly.scalajs.react.raw.React.{LazyResult, UnderlyingLazyResult}
 import japgolly.scalajs.react.vdom.VdomNode
-import japgolly.scalajs.react.{Callback, CallbackTo, raw => Raw}
-import scala.concurrent.duration.FiniteDuration
+import japgolly.scalajs.react.{AsyncCallback, raw => Raw}
 import scala.scalajs.js
 
 final class Lazy(val raw: Raw.React.Lazy) extends AnyVal
 
-object Lazy extends LazyDsl {
+object Lazy {
 
-  def fromJs(raw: Raw.React.Lazy): Lazy =
-    new Lazy(raw)
+  def apply(n: => VdomNode): Lazy =
+    apply(AsyncCallback.point(n).delayMs(0))
 
-  override def promise(f: (VdomNode => Callback) => Callback): Lazy = {
-    val p: () => js.Promise[LazyResult] =
-      () => new js.Promise[LazyResult]((resolve, _) =>
-        f(node => Callback(resolve(toRawResult(node)))).runNow())
+  def apply(a: AsyncCallback[VdomNode]): Lazy = {
+    val p = () => a.map(toRawResult).unsafeToJsPromise()
     fromJs(Raw.React.`lazy`(p))
   }
 
@@ -24,27 +21,7 @@ object Lazy extends LazyDsl {
     val node: UnderlyingLazyResult = input.rawNode
     js.Dynamic.literal(default = node.asInstanceOf[js.Any]).asInstanceOf[LazyResult]
   }
-}
 
-trait LazyDsl extends Any {
-  def promise(f: (VdomNode => Callback) => Callback): Lazy
-
-  final def apply(n: => VdomNode): Lazy =
-    fromCallback(CallbackTo(n))
-
-  final def fromCallback(c: CallbackTo[VdomNode]): Lazy =
-    promise(c.flatMap)
-
-  final def delay(by: FiniteDuration): LazyDsl.Delay =
-    delayMs(by.toMillis)
-
-  final def delayMs(ms: Double): LazyDsl.Delay =
-    new LazyDsl.Delay(ms)
-}
-
-object LazyDsl {
-  final class Delay(private val ms: Double) extends AnyVal with LazyDsl {
-    override def promise(f: (VdomNode => Callback) => Callback): Lazy =
-      Lazy.promise(resolve => f(resolve).delayMs(ms).void)
-  }
+  def fromJs(raw: Raw.React.Lazy): Lazy =
+    new Lazy(raw)
 }
