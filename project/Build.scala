d69634b71025dbb97ab95ef8eb3fe25b2e63067e
Scala Steward <me@scala-steward.org>
2020-01-21 16:31:55 +0100

    Update discipline-scalatest to 1.0.0-RC4

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object ScalajsReact {
     val BetterMonadicFor      = "0.3.1"
     val Cats                  = "2.0.0"
     val CatsTestkitScalaTest  = "1.0.0-RC1"
-    val DisciplineScalaTest   = "1.0.0-RC1"
+    val DisciplineScalaTest   = "1.0.0-RC4"
     val KindProjector         = "0.11.0"
     val MacroParadise         = "2.1.1"
     val MonocleCats           = "2.0.1"
