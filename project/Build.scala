947bb5c63d4693499f14d162abc30d2ba5ab0217
David Barri <japgolly@gmail.com>
2020-02-29 14:42:58 +1100

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object ScalajsReact {
   object Ver {
     val BetterMonadicFor      = "0.3.1"
     val Cats                  = "2.1.1"
-    val CatsTestkitScalaTest  = "1.0.0"
+    val CatsTestkitScalaTest  = "1.0.1"
     val DisciplineScalaTest   = "1.0.0-RC1"
     val KindProjector         = "0.11.0"
     val MacroParadise         = "2.1.1"
