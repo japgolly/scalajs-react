a2f0877c2cc1c173aaae8af817e7d138100138eb
Scala Steward <me@scala-steward.org>
2020-07-27 08:20:12 +0200

    Update cats-testkit-scalatest to 2.0.0

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,7 @@ object ScalajsReact {
     val BetterMonadicFor      = "0.3.1"
     val Cats                  = "2.1.1"
     val CatsEffect            = "2.1.3"
-    val CatsTestkitScalaTest  = "1.0.1"
+    val CatsTestkitScalaTest  = "2.0.0"
     val DisciplineScalaTest   = "1.0.1"
     val KindProjector         = "0.11.0"
     val MacroParadise         = "2.1.1"
