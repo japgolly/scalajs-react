00d46a0b213ce391c90607bb410e20fd75ae0d66
Scala Steward <me@scala-steward.org>
2021-01-20 10:38:13 +0100

    Update scalaz-effect to 7.2.31

--- project/Build.scala
+++ project/Build.scala
@@ -36,7 +36,7 @@ object ScalajsReact {
     val ScalaJsDom            = "1.1.0"
     val ScalaJsTime           = "1.0.0"
     val ScalaTest             = "3.1.4"
-    val Scalaz72              = "7.2.30"
+    val Scalaz72              = "7.2.31"
     val SizzleJs              = "2.3.0"
     val Sourcecode            = "0.2.1"
   }
