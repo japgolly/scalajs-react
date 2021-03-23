7b7a1d861a51748a5b201242b9181b0cd1bd5f6c
David Barri <japgolly@gmail.com>
2021-03-23 11:08:35 +1100

    Remove internal.JsSet
    Closes #851

--- core/src/main/scala/japgolly/scalajs/react/raw/Profiler.scala
+++ core/src/main/scala/japgolly/scalajs/react/raw/Profiler.scala
@@ -1,6 +1,5 @@
 package japgolly.scalajs.react.raw
 
-import japgolly.scalajs.react.internal.JsSet
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSGlobal
 
@@ -15,13 +14,13 @@ trait Interaction extends js.Object {
 object Profiler {
 
   type OnRender = js.Function7[
-    String,             // id:             the "id" prop of the Profiler tree that has just committed
-    String,             // phase:          either "mount" (if the tree just mounted) or "update" (if it re-rendered)
-    Double,             // actualDuration: time spent rendering the committed update
-    Double,             // baseDuration:   estimated time to render the entire subtree without memoization
-    Double,             // startTime:      when React began rendering this update
-    Double,             // commitTime:     when React committed this update
-    JsSet[Interaction], // interactions:   Set of interactions belonging to this update
+    String,              // id:             the "id" prop of the Profiler tree that has just committed
+    String,              // phase:          either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+    Double,              // actualDuration: time spent rendering the committed update
+    Double,              // baseDuration:   estimated time to render the entire subtree without memoization
+    Double,              // startTime:      when React began rendering this update
+    Double,              // commitTime:     when React committed this update
+    js.Set[Interaction], // interactions:   Set of interactions belonging to this update
     Unit
   ]
 
