6d897921b42d7364269de6b4c49ddceb3524f908
David Barri <japgolly@gmail.com>
2021-07-07 15:52:35 +1000

    Revert "Hmmm"
    This reverts commit 41b6da4b71911892405d7bff91f11154cce8d256.

--- coreGeneric/src/main/scala/japgolly/scalajs/react/internal/ShouldComponentUpdateComponent.scala
+++ coreGeneric/src/main/scala/japgolly/scalajs/react/internal/ShouldComponentUpdateComponent.scala
@@ -1,10 +1,8 @@
 package japgolly.scalajs.react.internal
 
-import japgolly.scalajs.react.{CtorType, Reusability}
-import japgolly.scalajs.react.component.Scala
+import japgolly.scalajs.react.Reusability
 import japgolly.scalajs.react.component.builder.EntryPoint
 import japgolly.scalajs.react.vdom.VdomNode
-import scala.language.`3.0`
 
 object ShouldComponentUpdateComponent {
 
@@ -13,22 +11,11 @@ object ShouldComponentUpdateComponent {
   private implicit val reusability: Reusability[Props] =
     Reusability.by(_._1)
 
-  private inline def createComponent() =
-    EntryPoint[Props]("ShouldComponentUpdate")
+  val Component =
+    EntryPoint[Props]("")
       .render_P(_._2())
       .configure(Reusability.shouldComponentUpdate)
       .build
-
-  type Component = Scala.Component[Props, Unit, Unit, CtorType.Props]
-
-  private var _component: Component =
-    null
-
-  inline def Component: Component = {
-    if (_component == null)
-      _component = createComponent()
-    _component
-  }
 
   def apply(rev: Int, vdom: () => VdomNode): VdomNode =
     Component((rev, vdom))
