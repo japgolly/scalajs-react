417a73cf4e90f3ace7e84907a468a20045dde0d2
David Barri <japgolly@gmail.com>
2021-07-10 11:38:45 +1000

    Update update-gh-pages.sh for source maps

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -6,8 +6,9 @@ cd "$(dirname "$0")"
 ii=ghpages/html/prod.html
 io=index.html
 js=ghpages/res/ghpages.js
+files="$io $js $js.map"
 
-rm -f $io $js
+rm -f $files
 
 perl -pe '
     s!"res/!"ghpages/res/!g;
@@ -15,7 +16,7 @@ perl -pe '
   ' > $io < $ii
 
 sbt ghpages/fullOptJS
-git add $js $io
+git add $files
 git st
-echo "git commit -m 'Refresh ghpages'"
+echo "git commit -m 'Refresh ghpages' -- $files"
 echo
