9ad1d4a8d9e79d08567a11afb45eaf581b0f961c
Scala Steward <me@scala-steward.org>
2021-07-21 10:35:02 +0200

    Update compile-time, test-util, types to 3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     // Externally observable
     val cats                  = "2.6.1"
     val catsEffect            = "3.1.1"
-    val microlibs             = "2.6"
+    val microlibs             = "3.0"
     val monocle2              = "2.1.0"
     val monocle3              = "3.0.0"
     val scala2                = "2.13.6"
