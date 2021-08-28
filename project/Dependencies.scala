64674f68703e06d7312d5e865e1dedc442feb58f
Scala Steward <me@scala-steward.org>
2021-08-29 01:13:35 +0200

    Update cats-effect, cats-effect-laws, ... to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
     // Externally observable
     val cats                  = "2.6.1"
-    val catsEffect            = "3.2.3"
+    val catsEffect            = "3.2.5"
     val microlibs             = "3.0.1"
     val monocle2              = "2.1.0"
     val monocle3              = "3.0.0"
