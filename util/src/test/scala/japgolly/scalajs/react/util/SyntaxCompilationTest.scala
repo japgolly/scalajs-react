5ae4a648dc11cd7f485b637154b0604c628e9ffc
David Barri <japgolly@gmail.com>
2021-11-04 13:20:01 +1100

    Add implicit syntax for generic effects

---
+++ util/src/test/scala/japgolly/scalajs/react/util/SyntaxCompilationTest.scala
@@ -0,0 +1,26 @@
+package japgolly.scalajs.react.util
+
+import japgolly.scalajs.react.util.syntax._
+
+trait SyntaxCompilationTest {
+
+  def sync[F[_]: Effect.Sync](f: F[Int]) =
+    for {
+      i <- f
+      j <- f
+    } yield {
+      f.reset.dispatch()
+      val k = f.runSync()
+      i + j + k
+    }
+
+  def async[F[_]: Effect.Async](f: F[Int]) =
+    for {
+      i <- f
+      j <- f
+    } yield {
+      val _ = f.toJsPromise
+      i + j
+    }
+
+}
