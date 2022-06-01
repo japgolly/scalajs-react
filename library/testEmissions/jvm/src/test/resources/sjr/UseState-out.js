f0e130f1ddfb01bcec456b5ba9ed50c625d54f63
David Barri <japgolly@gmail.com>
2022-06-01 21:52:12 +1000

    Update emissions for SJS 1.10

--- library/testEmissions/jvm/src/test/resources/sjr/UseState-out.js
+++ library/testEmissions/jvm/src/test/resources/sjr/UseState-out.js
@@ -3,35 +3,38 @@ function $c_Lsjr_test_emissions_UseState$() {
   $n_Lsjr_test_emissions_UseState$ = this;
   $j_sjr_package$.$m_Lsjr_package$();
   var this$4 = $j_sjr_hooks_HookComponentBuilder$.$m_Lsjr_hooks_HookComponentBuilder$().apply__Lsjr_hooks_HookComponentBuilder$ComponentP$First();
-  var initialState = new $j_AnonFunction0.$c_sjsr_AnonFunction0((this$2 => () => 123)(this));
+  var initialState = new $j_AnonFunction0.$c_sjsr_AnonFunction0(() => 123);
   var step = new $j_sjr_hooks_HookComponentBuilder$ComponentP$FirstStep.$c_Lsjr_hooks_HookComponentBuilder$ComponentP$FirstStep();
-  var this$18 = $j_sjr_hooks_Api$SecondaryWithRender.$as_Lsjr_hooks_Api$SecondaryWithRender($j_sjr_hooks_Api$Primary.$f_Lsjr_hooks_Api$Primary__useState__F0__Lsjr_hooks_Api$AbstractStep__O(this$4, initialState, step));
-  var f = new $j_AnonFunction2.$c_sjsr_AnonFunction2((this$2$1 => (x$1$2, s$2) => {
-    $j_java_lang_Object.$as_jl_Void(x$1$2);
-    var s = $j_sjr_hooks_Hooks$UseStateF.$as_Lsjr_hooks_Hooks$UseStateF(s$2);
-    var $$x2 = $j_sjr_vdom_HtmlTagOf$.$m_Lsjr_vdom_HtmlTagOf$();
+  var this$16 = $j_sjr_hooks_Api$SecondaryWithRender.$as_Lsjr_hooks_Api$SecondaryWithRender($j_sjr_hooks_Api$Primary.$f_Lsjr_hooks_Api$Primary__useState__F0__Lsjr_hooks_Api$AbstractStep__O(this$4, initialState, step));
+  var f = new $j_AnonFunction2.$c_sjsr_AnonFunction2((_$1, s) => {
+    $j_java_lang_Object.$as_jl_Void(_$1);
+    var s$1 = $j_sjr_hooks_Hooks$UseStateF.$as_Lsjr_hooks_Hooks$UseStateF(s);
+    var this$14 = $j_sjr_vdom_HtmlTagOf$.$m_Lsjr_vdom_HtmlTagOf$();
     $j_sjr_vdom_html_$.$m_Lsjr_vdom_html＿$();
     $j_sjr_vdom_Exports$.$m_Lsjr_vdom_Exports$();
+    var self = "button";
+    var $$x2 = $j_java_lang_Character$.$m_sr_ScalaRunTime$();
     $j_sjr_vdom_html_$.$m_Lsjr_vdom_html＿$();
     $j_sjr_vdom_VdomNode$.$m_Lsjr_vdom_VdomNode$();
     var $$x1 = new $j_sjr_vdom_VdomNode$$anon$1.$c_Lsjr_vdom_VdomNode$$anon$1("Count is ");
     $j_sjr_vdom_html_$.$m_Lsjr_vdom_html＿$();
-    var v = $j_java_lang_Object.$uI(s.Lsjr_hooks_Hooks$UseStateF$$anon$2__f_raw[0]);
+    var v = $j_java_lang_Object.$uI(s$1.Lsjr_hooks_Hooks$$anon$2__f_raw[0]);
     $j_sjr_vdom_VdomNode$.$m_Lsjr_vdom_VdomNode$();
-    var array = [$$x1, new $j_sjr_vdom_VdomNode$$anon$1.$c_Lsjr_vdom_VdomNode$$anon$1(v), $j_sjr_vdom_html_$.$m_Lsjr_vdom_html＿$().Lsjr_vdom_html＿$__f_$up.Lsjr_vdom_HtmlAttrAndStyles$__f_onClick.$minus$minus$greater__F0__F1__Lsjr_vdom_TagMod(new $j_AnonFunction0.$c_sjsr_AnonFunction0(((this$11, s$1) => () => {
+    var xs = $$x2.wrapRefArray__AO__sci_ArraySeq(new ($j_sjr_vdom_TagMod.$d_Lsjr_vdom_TagMod.getArrayOf().constr)([$$x1, new $j_sjr_vdom_VdomNode$$anon$1.$c_Lsjr_vdom_VdomNode$$anon$1(v), $j_sjr_vdom_html_$.$m_Lsjr_vdom_html＿$().Lsjr_vdom_html＿$__f_$up.Lsjr_vdom_HtmlAttrAndStyles$__f_onClick.$minus$minus$greater__F0__F1__Lsjr_vdom_TagMod(new $j_AnonFunction0.$c_sjsr_AnonFunction0((s$2 => () => {
       $j_sjr_Reusable$.$m_Lsjr_Reusable$();
-      var r = $j_sjr_hooks_Hooks$UseStateF.$f_Lsjr_hooks_Hooks$UseStateF__modState__Lsjr_Reusable(s$1);
-      return new $j_sjr_callback_CallbackTo.$c_Lsjr_callback_CallbackTo($j_sjr_callback_CallbackTo.$as_Lsjr_callback_CallbackTo($j_scala_Function1.$as_F1(r.value__O()).apply__O__O(new $j_scala_Function1.$c_sjsr_AnonFunction1((this$13 => x$2$2 => {
-        var x$2 = $j_java_lang_Object.$uI(x$2$2);
-        return 1 + x$2 | 0;
-      })(this$11)))).Lsjr_callback_CallbackTo__f_japgolly$scalajs$react$callback$CallbackTo$$trampoline);
-    })(this$2$1, s)), $j_sjr_vdom_Attr$EventCallback$.$m_Lsjr_vdom_Attr$EventCallback$().Lsjr_vdom_Attr$EventCallback$__f_defaultSync)];
-    return $$x2.apply$extension__T__sci_Seq__Lsjr_vdom_TagOf("button", new $j_WrappedVarArgs.$c_sjsr_WrappedVarArgs(array));
-  })(this));
-  var this$17 = $j_sjr_hooks_HookComponentBuilder$ComponentP$SubsequentStep$.$m_Lsjr_hooks_HookComponentBuilder$ComponentP$SubsequentStep$();
-  var step$1 = new $j_sjr_hooks_ComponentP_SubsequentSteps$$anon$1.$c_Lsjr_hooks_ComponentP＿SubsequentSteps$$anon$1(this$17);
+      var r = $j_sjr_hooks_Hooks$UseStateF.$f_Lsjr_hooks_Hooks$UseStateF__modState__Lsjr_Reusable(s$2);
+      var ev$1 = $j_scala_Function1.$as_F1(r.value__O()).apply__O__O(new $j_scala_Function1.$c_sjsr_AnonFunction1(_$2 => {
+        var _$2$1 = $j_java_lang_Object.$uI(_$2);
+
+        return 1 + _$2$1 | 0;
+      }));
+      return new $j_sjr_callback_CallbackTo.$c_Lsjr_callback_CallbackTo(ev$1 === null ? null : $j_sjr_callback_CallbackTo.$as_Lsjr_callback_CallbackTo(ev$1).Lsjr_callback_CallbackTo__f_trampoline);
+    })(s$1)), $j_sjr_vdom_Attr$EventCallback$.$m_Lsjr_vdom_Attr$EventCallback$().Lsjr_vdom_Attr$EventCallback$__f_defaultSync)]));
+    return $j_sjr_vdom_TagLite.$f_Lsjr_vdom_TagLite__apply__T__sci_Seq__Lsjr_vdom_TagOf(this$14, self, xs);
+  });
+  var step$1 = new $j_sjr_hooks_ComponentP_SubsequentSteps$$anon$1.$c_Lsjr_hooks_ComponentP＿SubsequentSteps$$anon$1();
   var s$3 = $j_sjr_CtorType$Summoner$.$m_Lsjr_CtorType$Summoner$().summonN__Lsjr_internal_Singleton__Lsjr_CtorType$Summoner($j_sjr_internal_Singleton$.$m_Lsjr_internal_Singleton$().Lsjr_internal_Singleton$__f_BoxUnit);
-  this.Lsjr_test_emissions_UseState$__f_Component = $j_sjr_hooks_Api$SecondaryWithRender.$f_Lsjr_hooks_Api$SecondaryWithRender__render__O__Lsjr_hooks_Api$SubsequentStep__Lsjr_CtorType$Summoner__Lsjr_component_JsBaseComponentTemplate$ComponentWithRoot(this$18, f, step$1, s$3);
+  this.Lsjr_test_emissions_UseState$__f_Component = $j_sjr_hooks_Api$SecondaryWithRender.$f_Lsjr_hooks_Api$SecondaryWithRender__render__O__Lsjr_hooks_Api$SubsequentStep__Lsjr_CtorType$Summoner__Lsjr_component_JsBaseComponentTemplate$ComponentWithRoot(this$16, f, step$1, s$3);
 }
 
 export { $c_Lsjr_test_emissions_UseState$ };
