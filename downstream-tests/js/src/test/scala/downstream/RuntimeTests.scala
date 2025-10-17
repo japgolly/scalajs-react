22f8ae9007250688f3a414bd2bba9379de752875
David Barri <japgolly@gmail.com>
2025-10-17 14:33:17 +1100

    Bugfix for testWarningsReact test

--- downstream-tests/js/src/test/scala/downstream/RuntimeTests.scala
+++ downstream-tests/js/src/test/scala/downstream/RuntimeTests.scala
@@ -96,8 +96,8 @@ object RuntimeTests extends AsyncTestSuite {
       "react" - {
         val c = ScalaFnComponent[Int](i => <.p(<.td(s"i = $i")))
         ReactTestUtils.withRendered_(c(123))(_ => ()).attemptTry.map { t =>
-          assertEq(t.isFailure, testWarningsReact.contains("react"))
-          t.toEither
+          assertEq(t.isFailure, testWarningsReact.contains("fatal"))
+          (testWarningsReact, t.toEither)
         }
       }
 
