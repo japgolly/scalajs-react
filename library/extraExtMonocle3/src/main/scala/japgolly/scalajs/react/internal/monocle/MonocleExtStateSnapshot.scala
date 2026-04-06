d952338b05c8af906ff7220fff4ae40c7d1b72eb
David Barri <japgolly@gmail.com>
2026-04-07 09:50:35 +1000

    Restore some deprecated methods for clarity
    We don't want someone re-adding these reasonable-looking methods back in future

--- library/extraExtMonocle3/src/main/scala/japgolly/scalajs/react/internal/monocle/MonocleExtStateSnapshot.scala
+++ library/extraExtMonocle3/src/main/scala/japgolly/scalajs/react/internal/monocle/MonocleExtStateSnapshot.scala
@@ -1,6 +1,7 @@
 package japgolly.scalajs.react.internal.monocle
 
 import japgolly.scalajs.react.extra.{StateSnapshot, StateSnapshotF}
+import japgolly.scalajs.react.util.NotAllowed
 
 trait MonocleExtStateSnapshot {
   import MonocleExtStateSnapshot._
@@ -60,5 +61,14 @@ object MonocleExtStateSnapshot {
   }
 
   final class InstanceWithReuse[F[_], A[_], S](private val self: StateSnapshotF.InstanceMethodsWithReuse[F, A, S]) extends AnyVal {
+
+    @deprecated("This ability doesn't work. See https://github.com/japgolly/scalajs-react/issues/721 for an explanation, and https://japgolly.github.io/scalajs-react/#examples/state-snapshot-2 for the alternative.", "1.7.1")
+    def xmapStateL(no: NotAllowed) = no.result
+
+    @deprecated("This ability doesn't work. See https://github.com/japgolly/scalajs-react/issues/721 for an explanation, and https://japgolly.github.io/scalajs-react/#examples/state-snapshot-2 for the alternative.", "1.7.1")
+    def zoomStateL(no: NotAllowed) = no.result
+
+    @deprecated("This ability doesn't work. See https://github.com/japgolly/scalajs-react/issues/721 for an explanation, and https://japgolly.github.io/scalajs-react/#examples/state-snapshot-2 for the alternative.", "1.7.1")
+    def zoomStateO(no: NotAllowed) = no.result
   }
 }
