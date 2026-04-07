6720a2eb99594d09cafb543d37e7f7b433d36fa9
David Barri <japgolly@gmail.com>
2026-04-07 13:22:55 +1000

    Remove unused import

--- library/tests/src/test/scala/japgolly/scalajs/react/core/ReactTest.scala
+++ library/tests/src/test/scala/japgolly/scalajs/react/core/ReactTest.scala
@@ -2,7 +2,6 @@ package japgolly.scalajs.react.core
 
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.test.ReactTestUtils
-import japgolly.scalajs.react.test.TestUtil._
 import japgolly.scalajs.react.vdom.html_<^._
 import scala.scalajs.js
 import utest._
